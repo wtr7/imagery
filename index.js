700b35f9413d6407bff5cd2b77f6316cf23182f5
wtr77 <wouter_vanrespaille@hotmail.com>
2015-07-06 17:25:54 +0200

    handle errors + rewrite options

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var async = require('async');
+var _ = require('lodash');
 var mime = require('mime');
 var im = require('gm').subClass({
 	imageMagick: true
@@ -20,38 +21,26 @@ function imagery(currentDir, options, callback) {
 		throw new Error('No callback has been provided');
 	}
 
-	var destination = (options && options.destination) ? options.destination : path.resolve(currentDir, '..', 'edited_images');
-	var quality = (options && options.quality) ? options.quality : 35;
-	var resizeWidth = (options && options.resize && options.resize.width) ? options.resize.width : null;
-	var resizeHeight = (options && options.resize && options.resize.height) ? options.resize.height : null;
-	var scaleWidth = (options && options.scale && options.scale.width) ? options.scale.width : null;
-	var scaleHeight = (options && options.scale && options.scale.height) ? options.scale.height : null;
-	var compressType = (options && options.compressType) ? options.compressType : null;
-	var fileType = null;
-	if (options && options.fileType && (
-		options.fileType.toLowerCase() === 'jpeg' ||
-		options.fileType.toLowerCase() === 'jpg' ||
-		options.fileType.toLowerCase() === 'tiff' ||
-		options.fileType.toLowerCase() === 'tif' ||
-		options.fileType.toLowerCase() === 'png' ||
-		options.fileType.toLowerCase() === 'gif'
-	)) {
+	options = _.defaults(options) || {};
+
+	var destination = options.destination || path.resolve(currentDir, '..', 'edited_images')
+	var quality = options.quality || 35;
+	var resizeWidth = (options.resize && options.resize.width) ? options.resize.width : null;
+	var resizeHeight = (options.resize && options.scale.height) ? options.scale.height : null;
+	var scaleWidth = (options.scale && options.scale.width) ? options.scale.width : null;
+	var scaleHeight = (options.scale && options.scale.height) ? options.scale.height : null;
+	var compressType = options.compressType || null;
+	var fileType = 'jpeg';
+	if (options && options.fileType && _.contains(['jpeg', 'jpg', 'tiff', 'tif', 'png', 'gif'], options.fileType.toLowerCase())) {
 		fileType = options.fileType;
 	}
 
 	// if destination doesn't exists, create the directory and validate current path
 	fs.exists(destination, function(exists) {
-		if (exists) {
-			return validateCurrentPath();
-		}
+		if (exists) return validateCurrentPath();
 
 		fs.mkdir(destination, function(err) {
-			if (err) {
-				return callback({
-					message: 'Couldn\'t make directory',
-					path: destination
-				}, null, false);
-			}
+			if (err) return handleError(destination,'Can\'t make directory');
 
 			return validateCurrentPath();
 		});
@@ -60,67 +49,50 @@ function imagery(currentDir, options, callback) {
 	function validateCurrentPath() {
 		fs.exists(currentDir, function(exists) {
 			// check if current path exists
-			if (exists) {
-				return deepLinking(currentDir, destination);
-			}
+			if (exists) return deepLinking(currentDir, destination);
 
-			return callback({
-				message: 'Path doesn\'t exists',
-				path: currentDir
-			}, null, false);
+			return handleError(currentDir, 'Path doesn\'t exists');
 		});
 	}
 
 	function deepLinking(root, newDir) {
 		fs.stat(root, function(err, stats) {
-			if (err) {
-				return callback({
-					message: 'Path doesn\'t exists',
-					path: root
-				}, null, false);
-			}
+			if (err) return handleError(root, 'Path doesn\'t exists');
 
 			// if not a directory, handle the file
-			if (!stats.isDirectory()) {
-				return handleFile(root);
-			}
+			if (!stats.isDirectory()) return handleFile(root);
 
 			// if directy, loop through all files
 			fs.readdir(root, function(err, files) {
-				if (err) {
-					return callback({
-						message: 'Couldn\'t read directory',
-						path: root
-					}, null, false);
-				}
+				if (err) return handleError(root, 'Can\'t read directory');
 
 				// to check if all files are done, check total files with files compressed
 				totalFiles += files.length;
 				async.eachSeries(files, function(file, cb) {
-					filesChecked++;
 					var currentPath = path.join(root, file);
 
 					fs.stat(currentPath, function(err, stats) {
 						if (err) {
-							return callback({
-								message: 'Couldn\'t read directory',
-								path: currentPath
-							}, null, false);
+							filesChecked++;
+							return handleError(currentPath, 'Can\'t read directory');
 						}
 
 						// if current path is directory, keep going deeper
 						if (stats.isDirectory()) {
+							filesChecked++;
 							deepLinking(currentPath, path.join(newDir, file));
 							return cb();
 						}
 
 						//if not an image, go to next file
 						if (!~mime.lookup(currentPath).indexOf('image/')) {
+							filesChecked++;
 							return cb();
 						}
 
 						// if new dir doens't exists, create it
 						// if it does, write new file to it
+
 						fs.exists(newDir, function(exists) {
 							if (exists) {
 								return compress(currentPath, path.join(newDir, file), function(err, response, done) {
@@ -130,12 +102,7 @@ function imagery(currentDir, options, callback) {
 							}
 
 							fs.mkdir(newDir, function(err) {
-								if (err) {
-									return callback({
-										message: 'Couldn\'t make directory',
-										path: destination
-									}, null, false);
-								}
+								if (err) return handleError(newDir,'Can\'t make directory');
 
 								return compress(currentPath, path.join(newDir, file), function(err, response, done) {
 									cb();
@@ -147,6 +114,13 @@ function imagery(currentDir, options, callback) {
 				});
 			});
 		});
+	}
+
+	function handleError(path, message) {
+		return callback({
+			message: message,
+			path: path
+		}, null, true);
 	}
 
 	function handleFile(root) {
@@ -164,11 +138,7 @@ function imagery(currentDir, options, callback) {
 	}
 
 	function compress(oldPath, newPath, cb) {
-		var done = (filesChecked >= totalFiles) ? true : false;
-
-		if (fileType) {
-			newPath = newPath.split(path.extname(newPath))[0] + '.' + fileType;
-		}
+		if (fileType) newPath = newPath.split(path.extname(newPath))[0] + '.' + fileType;
 
 		var imageWriter = im(oldPath)
 			.compress(compressType)
@@ -183,6 +153,9 @@ function imagery(currentDir, options, callback) {
 		}
 
 		imageWriter.write(newPath, function(err, stdout, stderr) {
+			filesChecked++;
+			var done = (filesChecked >= totalFiles) ? true : false;
+
 			if (err || stderr) {
 				return cb('Writing file to: ', newPath + ' has failed', null, done);
 			}
